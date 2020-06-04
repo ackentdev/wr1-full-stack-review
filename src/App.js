9ea909c68f18fb9c7ca2ef6b3e0d6f185e104272
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-06-04 12:57:19 -0700

    updated store for middleware

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import routes from './routes';
 import {connect} from 'react-redux';
 import './App.css';
 
-function App() {
+function App(props) {
   console.log("look at me! ", props)
   return (
     <div className="App">
