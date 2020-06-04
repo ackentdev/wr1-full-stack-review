79c1d99c98eb93461af569c530f0fd79520026f9
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-06-04 13:59:17 -0700

    this is my final redux form!

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import {getUser} from '../redux/reducer';
 
 class Dashboard extends Component {
+
+    componentDidMount(){
+        this.props.getUser();
+    }
+
     render(){
         console.log('from dashboard now: ', this.props)
         return (
@@ -11,4 +17,4 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = reduxState => reduxState;
-export default connect(mapStateToProps)(Dashboard);
+export default connect(mapStateToProps, {getUser})(Dashboard);
