872b1a7db6b2bd1a172105a0723404ee9bc0fd67
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-06-04 12:52:03 -0700

    finished react-redux skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import routes from './routes';
+import {connect} from 'react-redux';
 import './App.css';
 
 function App() {
+  console.log("look at me! ", props)
   return (
     <div className="App">
      {routes}
@@ -11,4 +13,6 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps)(App)
